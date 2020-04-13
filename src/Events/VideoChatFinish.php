60ea6f01c7771a6cf66b1ac9cc593008024b5d3e
sobolevna <sobolevna@gmail.com>
2020-04-13 17:19:38 +0300

    Исправил работу окончания звонка Теперь запись останавливается, если соединений меньше трёх

---
+++ src/Events/VideoChatFinish.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sobolevna\LaravelVideoChat\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class VideoChatFinish implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $data;
+    public $channel;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param $data
+     * @param $channel
+     */
+    public function __construct($data, $channel)
+    {
+        $this->data = $data;
+        $this->channel = $channel;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PresenceChannel($this->channel);
+    }
+
+    public function broadcastWith()
+    {
+        return $this->data;
+    }
+}
--- src/Http/Controllers/CallController.php
+++ src/Http/Controllers/CallController.php
@@ -10,23 +10,43 @@ use SquareetLabs\LaravelOpenVidu\OpenVidu;
 use SquareetLabs\LaravelOpenVidu\Builders\RecordingPropertiesBuilder;
 use Storage;
 
+/**
+ * @todo Логику перенести в сервисные классы
+ */
 class CallController extends Controller
 {
     protected $openviduController = '\SquareetLabs\LaravelOpenVidu\Http\Controllers\OpenViduController';
     
+    /**
+     * @todo События сделать как положено
+     * @param int $id 
+     * @param Request $request
+     * @param Openvidu $manager
+     * @return \Illuminate\Http\Response
+     */
     public function start($id, Request $request, Openvidu $manager) {
         $session = $manager->getSession($id);
+        \broadcast(new VideoChatStart($request->all(), ''));
         if (!$session->getLastRecordingId()) {
             $recording = $manager->startRecording(RecordingPropertiesBuilder::build($request->all()));
             return response()->json(['recording' => $recording], 200);
         }
     }
 
+    /**
+     * @todo События сделать как положено
+     * @param int $id 
+     * @param Request $request
+     * @param Openvidu $manager
+     * @return \Illuminate\Http\Response
+     */
     public function finish($id, Request $request, Openvidu $manager) {
         $session = $manager->getSession($id);
         $lastRecordingId = $session->getLastRecordingId();
-        if ($lastRecordingId) {
-            $recording = $manager->startRecording($lastRecordingId);
+        $connections = $session->getActiveConnections();
+        \broadcast(new VideoChatFinish($request->all(), ''));
+        if ($lastRecordingId && count($connections)<3) {
+            $recording = $manager->stopRecording($lastRecordingId);
             return response()->json(['recording' => $recording], 200);
         }
     }
