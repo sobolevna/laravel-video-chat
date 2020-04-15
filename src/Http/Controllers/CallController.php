f804a143c84a713c30e43761af642acab6dbdce0
sobolevna <sobolevna@gmail.com>
2020-04-15 11:12:58 +0300

    Исправил ошибку при поломке  кэша laravel-openvidu

--- src/Http/Controllers/CallController.php
+++ src/Http/Controllers/CallController.php
@@ -11,6 +11,7 @@ use Sobolevna\LaravelVideoChat\Events\{
 use Illuminate\Routing\Controller;
 use SquareetLabs\LaravelOpenVidu\OpenVidu;
 use SquareetLabs\LaravelOpenVidu\Exceptions\OpenViduRecordingNotFoundException;
+use SquareetLabs\LaravelOpenVidu\Exceptions\OpenViduSessionCantRecordingException;
 use SquareetLabs\LaravelOpenVidu\Builders\RecordingPropertiesBuilder;
 use Storage;
 
@@ -28,14 +29,44 @@ class CallController extends Controller
      * @param Openvidu $manager
      * @return \Illuminate\Http\Response
      */
-    public function start($id, Request $request, Openvidu $manager) {
+    public function start($id, Request $request, OpenVidu $manager) {
         $session = $manager->getSession($id);
         \broadcast(new VideoChatStart($request->all(), ''));
         if (!$session->isBeingRecorded()) {
-            $recording = $manager->startRecording(RecordingPropertiesBuilder::build($request->all()));
-            return response()->json(['recording' => $recording], 200);
+            try{ 
+                $recording = $manager->startRecording(RecordingPropertiesBuilder::build($request->all()));         
+                $message = "Recording successfully started";
+            }
+            catch(OpenViduSessionCantRecordingException $e) {
+                $recording = $this->findLastRecording($id);
+                $session->setIsBeingRecorded(true);
+                $session->setLastRecordingId($recording['id']);  
+                $message = "Session has been recorded, but there wasn't any data of it";              
+            }
+            return response()->json(['recording' => $recording, "message" => $message], 200);       
         }
-        return response()->json(["message' => 'Call started with session already being recorded"], 200);
+        return response()->json(["message" => "Call started with session already being recorded"], 200);
+    }
+
+    /**
+     * @todo Вот это точно должно быть в сервисном классе
+    */
+    protected function findLastRecording($sessionId)
+    {
+        $videos = [];
+        $i = 0;
+        while (true) {
+            $video = [];
+            $videoId = $i > 0 ? "$sessionId-$i" : $sessionId;
+            if (!Storage::exists("video/$videoId/$videoId.mp4")) {
+                break;
+            }
+            $i++;
+            if (!Storage::exists("video/$videoId/$videoId.jpg")) {
+                return json_decode(Storage::get("video/$videoId/.recording.$videoId"), true);
+            }
+        }
+        throw new OpenViduRecordingNotFoundException('This session hasn\'t unfinished recordings');
     }
 
     /**
@@ -46,7 +77,7 @@ class CallController extends Controller
      * @param Openvidu $manager
      * @return \Illuminate\Http\Response
      */
-    public function finish($id, Request $request, Openvidu $manager) {
+    public function finish($id, Request $request, OpenVidu $manager) {
         $session = $manager->getSession($id);
         $lastRecordingId = $session->getLastRecordingId();
         $connectionsCount = intval($request->get('connectionsCount'));
